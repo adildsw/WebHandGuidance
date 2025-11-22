de7c9c121240a1830dd0a9ddf9e3f5130ef84e1c
Adil Rahman <adildsw@gmail.com>
2025-11-22 01:52:00 -0500

    fixed null marker vis bug (2)

--- src/pages/TaskExecutor/taskVisualizationSketch.ts
+++ src/pages/TaskExecutor/taskVisualizationSketch.ts
@@ -129,7 +129,7 @@ const taskVisualizationSketch: Sketch = (p5) => {
   };
 
   const drawHoldMarker = () => {
-    if (currentTarget === -1) return;
+    if (markers.length === 0 || currentTarget === -1) return;
 
     const isInsideTarget =
       activeWristPos &&
@@ -160,7 +160,7 @@ const taskVisualizationSketch: Sketch = (p5) => {
 
   const drawMarkers = () => {
     if (markers.length === 0 || currentTarget === -1) return;
-    
+
     p5.noFill();
     p5.stroke(255);
     p5.strokeWeight(2);
